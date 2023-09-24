d19f11048d2625508b1eb8574fb60dafa7164ef0
khadira1937 <okhadira@.com>
2023-09-23 18:01:04 -0700

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,72 +1,84 @@
 #!/usr/bin/python3
-import os
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine
-from models.base_model import Base
-from models.user import User
-from models.state import State
-from models.city import City
+"""
+DB storage engine definition
+"""
+
+import models
 from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-class DBStorage:
-    """engine DBStorage connecting to MysqlAlchemy"""
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
+
+class DBStorage():
+    """Database storage engine"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """The init method that initializes an engine"""
-        self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}:3306/{}".format(
-                os.getenv('HBNB_MYSQL_USER'),
-                os.getenv('HBNB_MYSQL_PWD'),
-                os.getenv('HBNB_MYSQL_HOST'),
-                os.getenv('HBNB_MYSQL_DB'),
-            ),
-            pool_pre_ping=True,
-        )
-        if os.getenv("HBNB_ENV") == "test":
+        """Constructor method for DBStorage class"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB))
+        if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """A method that gets all the objects of cls if not
-        None, then it gets all the objects of all classes"""
-        allObjects = {}
-        if cls is None:
-            classes = [User, State, City, Amenity, Place, Review]
+        """Query on the current database session.
+           Returns all objects depending of the class name (argument cls)"""
+        if cls:
+            objs = self.__session.query(classes[cls])
         else:
-            classes = [cls]
-        for cls in classes:
-            objects = self.__session.query(cls).all()
-            for obj in objects:
-                key = "{}.{}".format(obj.__class__.__name__, obj.id)
-                allObjects[key] = obj
-        return allObjects
+            objs = self.__session.query(State).all()
+            objs += self.__session.query(City).all()
+            objs += self.__session.query(User).all()
+            objs += self.__session.query(Place).all()
+            objs += self.__session.query(Amenity).all()
+            objs += self.__session.query(Review).all()
+
+        my_dict = {}
+        for obj in objs:
+            obj_id = '{}.{}'.format(type(obj).__name__, obj.id)
+            my_dict[obj_id] = obj
+        return my_dict
 
     def new(self, obj):
-        """A method that adds an obj to the current database
-        session"""
-        if obj is not None:
-            if not self.__session:
-                raise Exception("No Session!")
-            self.__session.add(obj)
-            self.__session.commit()
+        """Adds the object to the current database session"""
+        self.__session.add(obj)
 
     def save(self):
-        """A method that commits the current database session"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """A method that deletes the current database session"""
+        """Delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
-            self.__session.commit()
 
     def reload(self):
-        """A method that reloads a session. saves all the tables
-        and it uses scoped session"""
+        """Create all tables in the database"""
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
+
+    def close(self):
+        """Call remove() method on the private session attribute"""
+        self.__session.remove()
--- models/state.py
+++ models/state.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from models.city import City
-import os
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-STORAGE = os.getenv("HBNB_TYPE_STORAGE")
+
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    if STORAGE == "db":
-        id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City",
-                            backref="state",
-                            cascade="all, delete-orphan")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """Returns the cities"""
-        from models import storage
-        city_list = []
-        for key, value in storage.all(City).items():
-            if self.id == value.state_id:
-                city_list.append(value)
-        return city_list
+    def __init__(self, *args, **kwargs):
+        """intialize state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """getter method
+            Returns:
+            list: list of City instances with state_id equals
+                  to the current State.id
+            """
+            cities_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
