e9a63807abcbe986486e2388a228c42b3fcaf60f
khadira1937 <okhadira@gmail.com>
2023-10-25 23:21:00 +0200

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -12,21 +12,21 @@ def index():
         return 'Hello HBNB!'
 
 
-    @app.route('/hbnb')
-    def hbnb():
-            return 'HBNB'
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
 
 
-        @app.route('/c/<text>')
-        def get_c_text(text):
-                return "C " + str(text.replace("_", " "))
+@app.route('/c/<text>')
+def get_c_text(text):
+    return "C " + str(text.replace("_", " "))
 
 
-            @app.route('/python', defaults={"text": "is cool"})
-            @app.route('/python/<text>')
-            def python_text(text):
-                    return "Python " + str(text.replace("_", " "))
+@app.route('/python', defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def python_text(text):
+    return "Python " + str(text.replace("_", " "))
 
 
-                if __name__ == "__main__":
-                        app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
